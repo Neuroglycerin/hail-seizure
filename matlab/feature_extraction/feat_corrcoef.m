b4b3834cb2a0c44eae669e692645561b4ac51a11
Scott Lowe <scottlowe0@gmail.com>
2014-09-19 19:04:11 +0100

    Basic correlation and covariance feature extraction

---
+++ matlab/feature_extraction/feat_corrcoef.m
@@ -0,0 +1,24 @@
+% Correlation Coefficient feature
+% Return a feature vector for each segment of certain type
+function featM = feat_corrcoef(subj,typ)
+
+% Get a list of files
+[myfiles,mydir] = subjtyp2dirs(subj,typ);
+nSeg = length(myfiles);
+
+% Check number of channels in this dataset
+Dat = loadSegFile(fullfile(mydir,myfiles(1).name));
+nChn = size(Dat.data,1);
+
+% Initialise holding variable
+featM = nan(nSeg,nChn^2);
+
+% Loop over each segment, computing feature
+for iSeg=1:nSeg
+    % Load this segment
+    Dat = loadSegFile(fullfile(mydir,myfiles(iSeg).name));
+    % Compute covariance between channels
+    featM(iSeg,:) = reshape(corrcoef(Dat.data'),1,[]);
+end
+
+end
