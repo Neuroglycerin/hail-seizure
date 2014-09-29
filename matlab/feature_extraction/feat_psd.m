9849eba655b003fe214221ed1eb66dc445ea2cbd
Finlay Maguire <finlaymaguire@gmail.com>
2014-09-29 22:51:15 +0100

    dangerously crap code to calculate PSD per channel per segment using standard EEG bands, fairly certain some form of normalisation that is needed is absent, don\'t use without serious checking

--- matlab/feature_extraction/feat_psd.m
+++ matlab/feature_extraction/feat_psd.m
@@ -17,59 +17,56 @@ function featM = feat_psd(subj, typ, window, bins)
                       'alpha',      [ 8.0   12.0],  ...
                       'beta',       [12.0   30.0],  ...
                       'low_gamma',  [30.0   70.0],  ...
-                      'high_gamma', [70.0  180.0]); 
+                      'high_gamma', [70.0  180.0]);
     end
     
+    % Get segment files using io functions
+    [fnames, mydir, segIDs] = subjtyp2dirs(subj, typ);
+    
+    % Number of segments of this subj and typ
+    nSeg = length(fnames);
+
+    % Check number of channels and num_samples in this dataset
+    % Assumes nChn and freq is constant within subj and typ
+    Dat = loadSegFile(fullfile(mydir, fnames{1}));
+    [nChn, data_points] = size(Dat.data);
+
+    % Window size (hanning) for pwelch unless specified otherwise
+    % Sqrt of number of samples rounded up to nearest int
     if nargin < 3
-        window = 60;  
+           window_size = ceil(sqrt(length(Dat.data(1,:))));
+           window = hanning(window_size);
     end
-    
-% Get segment files using io functions
-[fnames, mydir, segIDs] = subjtyp2dirs(subj, typ);
 
-nSeg = length(fnames);
-
-% Check number of channels and num_samples in this dataset
-% Assumes nChn and freq is constant within subj and typ
-Dat = loadSegFile(fullfile(mydir, fnames{1}));
-[nChn, data_points] = size(Dat.data);
-
-% Num of rows per window (frequency * window size)
-subsection_length = int32(Dat.sampling_frequency * window); 
-
-% Within each frequency
-% band the power was summed over band frequencies to produce a
-% power-in-band (PIB) feature. These features were aggregated into a
-% feature vector containing 96 PIB values 
-
-% Initialise holding variable
-featM = nan(nSeg, nChn*length(fieldnames(bins)));
-
-% Loop over each segment, computing feature
-for iSeg=1:nSeg
-    % Load this segment
-    Dat = loadSegFile(fullfile(mydir, fnames{iSeg}));
-    
-    Nfft = [];
+    % Pwelch window to use no overlap setting
     overlap = 0;
-   
-    
-    for channel=1:nChn
-        
-        [Pxx, f] = pwelch(Dat.data(channel,:), 600, overlap, Nfft, Dat.fs);
 
-        % Bin the spectral densities according to bins
-    
-    % Return spectral densities
-     %[spectra,freq] = pwelch(x,window,overlap,Nfft,Fs,
-                            %range,plot_type,detrend,sloppy)%
-       
-%   The power of the signal in a given frequency band [\omega_1,\omega_2] can be calculated by integrating over positive and negative frequencies,
-% Use trapezoidal numerical integration to approximate power in each band
-% trapz(X,Y,DIM) integrates across dimension DIM
- %   of Y. The length of X must be the same as size(Y,DIM))
-%  pwelch() to get the spectrum and trapz() to
-    % featM(iSeg,:) = reshape(cov(Dat.data'),1,[]);
-end
+    % Length of FFT to use
+    Nfft = window_size;
 
+    
+    % Initialise holding variable
+    featM = []; 
+    %nan(nSeg, nChn*length(fieldnames(bins)));
+
+    % Loop over each segment, computing feature
+    for iSeg=1:nSeg
+        % Load this segment
+        Dat = loadSegFile(fullfile(mydir, fnames{iSeg}));
+    
+        for channel=1:nChn
+            [Pxx, f] = pwelch(Dat.data(channel,:), ...
+                              window, ...
+                              overlap, ...
+                              Nfft, ...
+                              Dat.fs);
+            
+             for i = fieldnames(bins)'
+                freq_bin = bins.(i{1});
+                idx = find(f>=freq_bin(1) & f<=freq_bin(2));
+                density = trapz(f(idx), Pxx(idx));
+                featM = [featM, density];
+             end
+        end
+    end
 end
