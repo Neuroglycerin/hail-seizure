29575c7e2efdb723a67017026f3d49157f6b32d2
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-03 22:50:24 +0100

    updated parser to handle modtyp

--- python/utils.py
+++ python/utils.py
@@ -10,17 +10,24 @@ with open('SETTINGS.json') as settings_fh:
 def parse_matlab_HDF5(subj, settings=json_settings):
     '''
     Parse h5 file from matlab into hierarchial dict containing np arrays
-    input: subj - subject name (e.g. Dog1),
+    input: subj - subject name (e.g. Dog_1),
            settings - parsed json settings in dict format (json.load)
     output: subject_dict containing data in hierarchial format
-                    e.g. Dog_1 = {'interictal':
-                                        {'feat1': featvector (np.array),
-                                         'feat2 : featvector (np.array),
-                                         ...},
-                                  'preictal':
-                                        {'feat1': featvector (np.array),
-                                         ...},
-                                   'test': ...}
+                    e.g. Dog_1 = {'raw' :
+                                      {'interictal':
+                                           {'feat1': featvector (np.array),
+                                            'feat2 : featvector (np.array),
+                                            ...},
+                                     'preictal':
+                                           {'feat1': featvector (np.array),
+                                            ...},
+                                      'test': ...}
+                                   'ica' :
+                                        {'interictal' : {...}
+                                         'preictal' : {...}
+                                         'test' : {...}
+                                        }
+                                   }
     '''
 
 
@@ -28,16 +35,19 @@ def parse_matlab_HDF5(subj, settings=json_settings):
     version = settings['VERSION']
 
     # open h5 read-only file for correct subj and version number
+
     h5_file_name = "{0}/{1}{2}.h5".format(feature_location, subj, version)
-    print h5_file_name
-    print feature_location
-    print subj
-    print version
     h5_from_matlab = h5py.File(h5_file_name, 'r')
 
     # parse h5 object into dict using nested comprehensions (see docstring
     # for struct)
-    subject_dict = {typ: {feature: h5_from_matlab[typ][feature].value for feature in h5_from_matlab[typ]} for typ in h5_from_matlab}
+    subject_dict = {modtyp:
+            {typ:
+                {feature:
+                    h5_from_matlab[modtyp][typ][feature].value
+                    for feature in h5_from_matlab[modtyp][typ]}
+                for typ in h5_from_matlab[modtyp]}
+            for modtyp in h5_from_matlab}
 
     # make sure h5 object is closed
     h5_from_matlab.close()
