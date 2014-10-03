43260092bbad208b7e030048587adf2790e26d46
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-04 00:49:00 +0100

    added an example dataset h5 to prototype the train.py

--- train.py
+++ train.py
@@ -1,1 +1,14 @@
-import python.utils as utils # utils file
+#!/usr/bin/env python
+
+import python.utils as utils #contains mainly parsers
+import json
+from sklearn.pipeline import Pipeline
+
+def get_data(subjects):
+    return [utils.parse_matlab_HDF5(subj_dataset) for subj_dataset in subjects]
+
+if __name__=='__main__':
+
+    settings = json.load(open('SETTINGS.json', 'r'))
+
+    data = get_data(settings['SUBJECTS'])
