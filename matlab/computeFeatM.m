83392caf10d9ebb8446346d8f19a6a6d160f5af8
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-04 19:13:31 +0100

    updated io features and feature generation to maintain segment names instead of concatenating into matrices

--- matlab/computeFeatM.m
+++ matlab/computeFeatM.m
@@ -1,7 +1,7 @@
 % func should be a handle to a feature function which accepts two inputs
 % the datastructure, Dat, and a structure of input parameters
 
-function [featM,outparams] = computeFeatM(featfunc, subj, ictyp, modtyp, inparams)
+function [featM, outparams] = computeFeatM(featfunc, subj, ictyp, modtyp, inparams)
 
 % Input handling ----------------------------------------------------------
 if nargin<5
@@ -21,26 +21,30 @@ nFle = length(fnames);
 
 % Process the first segment so we know what the output size should be
 % Load this segment
-Dat = loadSegFile(fullfile(mydir,fnames{1}));
+% Dat = loadSegFile(fullfile(mydir,fnames{1}));
 % Apply the preprocessing model
-Dat = ppfunc(Dat);
+% Dat = ppfunc(Dat);
 % Compute the feature
-[featV,outparams] = featfunc(Dat, inparams);
+% [featV,outparams] = featfunc(Dat, inparams);
 
 % Initialise the holding variable
-featM = nan(nFle,numel(featV));
+% featM = nan(nFle,numel(featV));
 % Add the feature for the first segment file
-featM(1,:) = featV;
+% featM(1,:) = featV;
 
 % Parallelised feature computation ----------------------------------------
 % Loop over each segment, computing feature and adding to matrix
-parfor iFle=2:nFle
+
+featM = cell(nFle, 2);
+
+for iFle=1:nFle
     % Load this segment
     Dat = loadSegFile(fullfile(mydir,fnames{iFle}));
     % Apply the preprocessing model
     Dat = ppfunc(Dat);
     % Compute the feature
-    featM(iFle,:) = featfunc(Dat, inparams);
+    feat_vec = featfunc(Dat, inparams);
+    featM(iFle, :) = {fnames{iFle}, feat_vec};
 end
 
 end
