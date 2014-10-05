74dd4394415f512586b1b53f657112e54ecf62bf
Scott Lowe <scottlowe0@gmail.com>
2014-10-05 18:46:08 +0100

    Added timing

--- preprocessing.m
+++ preprocessing.m
@@ -18,10 +18,26 @@ feature_funcs = {...
 
 
 for iFun = 1:numel(feature_funcs)
+    tic2 = tic;
     for iSub=1:numel(subj)
         for iIct = 1:numel(ictypes)
-            fprintf('Running feature %s on raw %s %s\n',func2str(feature_funcs{iFun}), subj{iSub}, ictypes{iIct});
+            fprintf('%s: Running feature %s on raw %s %s\n', datestr(now,30), func2str(feature_funcs{iFun}), subj{iSub}, ictypes{iIct});
+            tic1 = tic;
             getFeatAddToHDF5(feature_funcs{iFun}, subj{iSub}, ictypes{iIct}, 'raw')
+            tme = toc(tic1);
+            tme = tme/60/60;
+            hrs = floor(tme);
+            mins = (tme-hrs)*60;
+            secs = round(mod(mins,1)*60);
+            mins = floor(mins);
+            fprintf('took %d h %d m %d s \n',hrs,mins,secs);
         end
     end
+    tme = toc(tic2);
+    tme = tme/60/60;
+    hrs = floor(tme);
+    mins = (tme-hrs)*60;
+    secs = round(mod(mins,1)*60);
+    mins = floor(mins);
+    fprintf('Feature %s took %d h %d m %d s \n',func2str(feature_funcs{iFun}),hrs,mins,secs);
 end
