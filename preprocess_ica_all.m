4ba7cdfb4f2fa82aa3315e711a0627c6ba59a864
Scott Lowe <scottlowe0@gmail.com>
2014-10-06 18:17:20 +0100

    Added weights for ICA

--- preprocess_ica_all.m
+++ preprocess_ica_all.m
@@ -4,8 +4,8 @@
 % Recursively add all of matlab folder to matlab path
 addpath(genpath('matlab'));
 
-names = subjnames();
-%names = {'Patient_2'};
+%names = subjnames();
+names = {'Patient_1'};
 for i=1:length(names);
     preprocess_ica(names{i});
 end
