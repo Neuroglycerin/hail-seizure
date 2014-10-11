47b05ee5fde6a2e506139ad313df044e50b01498
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-11 19:46:11 +0100

    add train, test, and output csv builders from gg notebook to update utils.py

--- python/utils.py
+++ python/utils.py
@@ -50,10 +50,6 @@ def parse_matlab_HDF5(feat, settings=json_settings):
 
     return feature_dict
 
-def reformat_features(subject_dict, typ):
-    pass
-
-
 def serialise_trained_model(model, model_name, settings=json_settings):
     '''
     Serialise and compress trained sklearn model to repo
@@ -74,17 +70,22 @@ def read_trained_model(model_name, settings=json_settings):
 
     return joblib.load(settings['MODEL_PATH']+'/'+model_name)
 
-def buildtraining(subject,features,data):
-    """Function to build data structures for ML:
-    
-    * __Input__: subject, features
-    * __Output___: X feature matrix, y target vector
-    
-    It will not tell you which feature is which."""
+def build_training(subject, features, data):
+    '''
+    Build labelled data set for training
+    input : subject  (subject name string)
+            features (features to use)
+    output: X (feature matrix as np.array)
+            y (target vector as np.array)
+
+    Does not preserve feature names
+    '''
+
     # hacking this for later
     first = features[0]
     for feature in features:
         Xf = np.array([])
+
         # enumerate to get numbers for target vector:
         #     0 is interictal
         #     1 is preictal
@@ -92,7 +93,8 @@ def buildtraining(subject,features,data):
             for segment in data[feature][subject][ictal].keys():
                 # now stack up the feature vectors
                 try:
-                    Xf = np.vstack([Xf,np.ndarray.flatten(data[feature][subject][ictal][segment].T)])
+                    Xf = np.vstack([Xf,
+                                    np.ndarray.flatten(data[feature][subject][ictal][segment].T)])
                 except ValueError:
                     Xf = np.ndarray.flatten(data[feature][subject][ictal][segment].T)
                 # and stack up the target vector
@@ -104,12 +106,57 @@ def buildtraining(subject,features,data):
                         y = [i]
         # stick the X arrays together
         try:
-            X = np.hstack([X,Xf])
+            X = np.hstack([X, Xf])
         except NameError:
             X = Xf
         except ValueError:
             print(feature)
-            print(X.shape,Xf.shape)
+            print(X.shape, Xf.shape)
+
     # turn y into an array
     y = np.array(y)
-    return X,y
+    return X, y
+
+def build_test(subject, features, data):
+    '''
+    Function to build data structures for submission
+    input : subject  (subject name string)
+            features (features to use)
+            data (nested dicts datastructure of all feature extracted data)
+    output: X (feature matrix as np.array)
+            y (labels as np.array)
+    '''
+
+    Xd = {}
+    for feature in features:
+        for segment in data[feature][subject]['test'].keys():
+            fvector = np.ndarray.flatten(data[feature][subject]['test'][segment])
+            try:
+                Xd[segment] = np.hstack([Xd[segment], fvector])
+            except:
+                Xd[segment] = fvector
+
+    # make the X array and corresponding labels
+    segments = []
+    X = []
+    for segment in Xd.keys():
+        segments.append(segment)
+        X.append(Xd[segment])
+    X = np.vstack(X)
+    return X, segments
+
+def output_csv(prediction_dict, settings=settings):
+    '''
+    Parse the predictions and output them in the correct format
+    for submission to the output directory
+    input:  prediction_dict (dictionary of all predictions of the test data)
+            settings (the settings dict from parsing the json_object)
+    output: void
+    '''
+    output_file = '{0}/output_{1}.csv'.format(settings['SUBMISSION_PATH'],
+                                              settings['VERSION'])
+    with open(output_file, 'w') as output_fh:
+        csv_output = csv.writer(output_fh)
+        csv_output.writerow(['clip', 'preictal'])
+        for segment in prediction_dict.keys():
+            csv_output.writerow.keys([segment, str(prediction_dict[segment])])
