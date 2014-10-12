81fa90a9180451d01f1597efd6779ee5cecda5d1
Scott Lowe <scottlowe0@gmail.com>
2014-10-12 02:22:57 +0100

    Rerun ICA weight computation to ensure consistency with final version of ICA script

--- preprocess_ica_all.m
+++ preprocess_ica_all.m
@@ -1,11 +1,11 @@
 % cd to the repository, then run this command
-% matlab13 -nodisplay -nosplash -r "preprocess_ica_all"
+% nice -15 matlab13 -nodisplay -nosplash -r "preprocess_ica_all"
 
 % Recursively add all of matlab folder to matlab path
 addpath(genpath('matlab'));
 
-%names = subjnames();
-names = {'Patient_1'};
+names = subjnames();
+
 for i=1:length(names);
     preprocess_ica(names{i});
 end
