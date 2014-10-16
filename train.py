100f9135ed6ce90a0c7a23b96dbdcd96e878689b
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-16 20:08:22 +0100

    update train.py to read ica feats too

--- train.py
+++ train.py
@@ -11,4 +11,6 @@ if __name__=='__main__':
 
     settings = json.load(open('SETTINGS.json', 'r'))
     features = ['raw_' + feat_name + '_' for feat_name in settings['FEATURES']]
+    for feat in ['ica_' + feat_name + '_' for feat_name in settings['FEATURES']]:
+        features.append(feat)
     data = get_data(features)
