bb313118c5d405eeeea7161bd35d9fbb911ca6b4
Finlay Maguire <finlaymaguire@gmail.com>
2014-10-21 00:50:16 +0100

    add some horribly untested train.py code and utils to train, doesn't solve any of the problems we really discussed tonight but hopefully its a start for me to work on when awake tomorrow

--- train.py
+++ train.py
@@ -2,7 +2,6 @@
 
 import python.utils as utils #contains mainly parsers
 import json
-from sklearn.pipeline import Pipeline
 
 def get_data(features):
     return {feat_name: utils.parse_matlab_HDF5(feat_name) for feat_name in features}
@@ -10,7 +9,31 @@ def get_data(features):
 if __name__=='__main__':
 
     settings = json.load(open('SETTINGS.json', 'r'))
+
+
     features = ['raw_' + feat_name + '_' for feat_name in settings['FEATURES']]
     for feat in ['ica_' + feat_name + '_' for feat_name in settings['FEATURES']]:
         features.append(feat)
+
     data = get_data(features)
+
+    X,y = utils.build_training(settings['SUBJECTS'], features, data)
+
+    cv = utils.get_cross_validation_set(y)
+
+    selector = utils.get_selector(k=3000)
+
+    scaler = utils.get_scaler()
+
+    classifier = utils.get_classifier()
+
+    model_pipe = utils.get_model(selector, scaler, classifier)
+
+    for train, test in cv:
+        fitted_model = utils.fit_model(model_pipe,
+                                  X[train],
+                                  y[train],
+                                  cv,
+                                  clf__sample_weight=weights)
+
+        serialise_trained_model(fitted_model, "model_{0}".format(str(model_index)))
