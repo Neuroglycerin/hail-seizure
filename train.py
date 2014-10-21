a99fc87aeed4eb8dd41bae8e122ad4c3542c5d52
Gavin <gavingray1729@gmail.com>
2014-10-21 22:34:43 +0100

    should now do everything required, just waiting on the correct cv iterator

--- train.py
+++ train.py
@@ -16,8 +16,6 @@ if __name__=='__main__':
 
     subjects = settings['SUBJECTS']
 
-    X,y = utils.build_training(subjects[0], features, data)
-
     # this cross-val is broken at the moment, for reasons discussed in the meeting
     cv = utils.get_cross_validation_set(y)
 
@@ -32,12 +30,41 @@ if __name__=='__main__':
     #model_pipe = utils.get_model([('thr',thresh),('sel',selector),('scl',scaler),('cls',classifier)])
     model_pipe = utils.get_model([('scl',scaler),('cls',classifier)])
 
-    for (train, test) ,subject in zip(cv,subjects):
-        fitted_model = utils.fit_model(model_pipe,
-                                  X[train],
-                                  y[train],
-                                  cv,
-                                  clf__sample_weight=weights)
+    for subject in subjects:
 
-        serialise_trained_model(fitted_model, "model_{0}_{1}".format(subject,setting["VERSION"]))
+        X,y = utils.build_training(subjects[0], features, data)
 
+        # initialise lists for cross-val results
+        predictions = []
+        labels = []
+        allweights = []
+
+        # run cross validation and report results
+        for (train, test), subject in zip(cv,subjects):
+            # calculate the weights
+            weights = utils.get_weights(y[train])
+            # fit the model to the training data
+            model_pipe.fit(X[train],y[train],clf__sample_weight=weights)
+            # append new predictions
+            predictions.append(model.predict_proba(X[test]))
+            # append test weights to store (why?)
+            weights = utils.get_weights(y[test])
+            allweights.append(weights)
+            # store true labels
+            labels.append(y[test])
+
+        # stack up the results
+        predictions = utils.np.vstack(predictions)[:,1]
+        labels = utils.np.hstack(labels)
+        weights = utils.np.hstack(allweights)
+
+        # calculate the total AUC score
+        auc = utils.sklearn.metrics.roc_auc_score(labels,predictions,sample_weight=weights)
+        print("predicted AUC score: {0}".format(auc))
+
+        # fit the final model
+        weights = utils.get_weights(y)
+
+        # save it
+        model_pipe.fit(X,y,clf__sample_weight=weights)
+        serialise_trained_model(model_pipe, "model_{0}_{1}".format(subject,setting["VERSION"]))
