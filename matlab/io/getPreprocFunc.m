226329be0a93419e7cdbbc31e4fd7aa8c34d782b
Scott Lowe <scottlowe0@gmail.com>
2014-11-04 19:37:02 +0000

    Added line noise removal and downsampling

--- matlab/io/getPreprocFunc.m
+++ matlab/io/getPreprocFunc.m
@@ -6,15 +6,32 @@ if ~ischar(modtyp);
 end
 
 % Main --------------------------------------------------------------------
-switch lower(modtyp)
+% Commas separate different preprocessing functions to use
+K = strfind(modtyp,',');
+if ~isempty(K)
+    submodtyp = modtyp(1:K(1)-1);
+else
+    submodtyp = modtyp;
+end
+
+switch lower(submodtyp)
     case 'raw'
         func = @(x)x;
     case 'ica'
         func = @(x) raw2ica(x, subj);
     case 'csp'
         func = @(x) raw2csp(x, subj);
+    case 'cln'
+        func = @(x) raw2cln(x);
+    case 'dwn'
+        func = @(x) raw2dwn(x);
     otherwise
-        error('Unfamiliar model preprocessing: %s',modtyp);
+        error('Unfamiliar model preprocessing: %s',submodtyp);
+end
+
+if ~isempty(K)
+    func2 = getPreprocFunc(modtyp(K(1)+1:end), subj);
+    func = @(x) func2(func(x));
 end
 
 end
---
+++ matlab/io/raw2cln.m
@@ -0,0 +1,27 @@
+function Dat = raw2cln(Dat)
+
+% Parameters
+order = 2;
+
+% No need to clean if sampling rate is low
+if Dat.fs<600
+    return;
+end
+
+% Note: apply the same preprocessing model to every dataset
+
+% Remove 60Hz line noise artifact (present in Patient_1)
+% Notch reject
+Dat.data = butterfiltfilt(Dat.data', [55 65], Dat.fs, order, 'stop', 'both')';
+
+% Remove 180Hz line noise artifact harmonic (present in Patient_1)
+Dat.data = butterfiltfilt(Dat.data', [175 185], Dat.fs, order, 'stop', 'both')';
+
+% Remove 240Hz line noise artifact harmonic (present in Patient_1)
+Dat.data = butterfiltfilt(Dat.data', [235 245], Dat.fs, order, 'stop', 'both')';
+
+% Remove <1Hz (present in Patient_2)
+% High-pass filter
+Dat.data = butterfiltfilt(Dat.data', [0.1 Inf], Dat.fs, order)';
+
+end
