de04c6f16e8d3da678bac44f60f05460d196981e
Scott Lowe <scottlowe0@gmail.com>
2014-11-05 14:22:02 +0000

    Processing ICA weights for cleaned data

--- matlab/io/raw2cln.m
+++ matlab/io/raw2cln.m
@@ -4,7 +4,7 @@ function Dat = raw2cln(Dat)
 order = 2;
 
 % No need to clean if sampling rate is low
-if Dat.fs<600
+if Dat.sampling_frequency<600
     return;
 end
 
@@ -12,16 +12,16 @@ end
 
 % Remove 60Hz line noise artifact (present in Patient_1)
 % Notch reject
-Dat.data = butterfiltfilt(Dat.data', [55 65], Dat.fs, order, 'stop', 'both')';
+Dat.data = butterfiltfilt(Dat.data', [55 65], Dat.sampling_frequency, order, 'stop', 'both')';
 
 % Remove 180Hz line noise artifact harmonic (present in Patient_1)
-Dat.data = butterfiltfilt(Dat.data', [175 185], Dat.fs, order, 'stop', 'both')';
+Dat.data = butterfiltfilt(Dat.data', [175 185], Dat.sampling_frequency, order, 'stop', 'both')';
 
 % Remove 240Hz line noise artifact harmonic (present in Patient_1)
-Dat.data = butterfiltfilt(Dat.data', [235 245], Dat.fs, order, 'stop', 'both')';
+Dat.data = butterfiltfilt(Dat.data', [235 245], Dat.sampling_frequency, order, 'stop', 'both')';
 
 % Remove <1Hz (present in Patient_2)
 % High-pass filter
-Dat.data = butterfiltfilt(Dat.data', [0.1 Inf], Dat.fs, order)';
+Dat.data = butterfiltfilt(Dat.data', [0.1 Inf], Dat.sampling_frequency, order)';
 
 end
