310a3f76e3353790663ac1e3b16bfd61fb01e3db
Scott Lowe <scottlowe0@gmail.com>
2014-11-10 16:41:43 +0000

    Running ICA on CSP-dim-reduced data

--- preprocess_ica.m
+++ preprocess_ica.m
@@ -4,8 +4,8 @@
 % Recursively add all of matlab folder to matlab path
 addpath(genpath('matlab'));
 
-epsilon = 1e-9;
-extramodtyp = 'cln,dwn';
+epsilon = 1e-12;
+extramodtyp = 'cln,cspdr,dwn';
 names = subjnames();
 
 for i=1:length(names);
