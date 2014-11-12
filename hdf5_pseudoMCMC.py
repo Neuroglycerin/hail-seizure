cd00184b646a9ff0c226b2aa4bba7ef927e525fe
Gavin <gavingray1729@gmail.com>
2014-11-12 13:31:13 +0000

    first version of stochastic optimisation

--- hdf5_pseudoMCMC.py
+++ hdf5_pseudoMCMC.py
@@ -1,38 +1,110 @@
 # coding: utf-8
 
+import json
+import train
+import random
+import os
+from generatebatchsettings import *
+import numpy as np
+import copy
+import hashlib
+import glob
 
-
-def main():
+def main(start=None,verbose=False):
     """
     Contains the main loop for this script.
     Pseudo-MHMCMC to find optimal AUC scoring 
     combinations of HDF5s.
+    start - location of json file settings to begin at
     """
     # pseudo-code for the MCMC iteration
     # want it to start with the probably good features
-    # Sample a new hdf5 and replace existing at random
-    #   Or, just push it in, or just drop a hdf5 at random
-    # Evaluate the AUC for this, run train.py
-    # compute acceptance probability from AUC:
-    #     r = min(1,AUC/(previous AUC))
-    # accept new point with probability r
+    if start == None:
+        with open("startchain_gavin.json") as f:
+            start = json.load(f)
+        startauc = 0.86575473285
+    mcmcdir = "hdf5mcmc"
 
-    # Should we use log loss instead of AUC to maintain probabilistic interpretation?
-    # Not so familiar with log loss.
-    # Seems this method will at least search for optimum AUC scores, which is the aim.
+    # hardcode AUC results to the hdf5mcmc directory
+    start['AUC_SCORE_PATH'] = mcmcdir
 
+    # have to load a list of possible features to replace with
+    featlist = get_featlist()
+    # and possible preceding modifiers
+    modlist  = get_modlist()
 
-for i in range(20):
-    newjson = {}
-    newjson.update(start)
-    features = start['FEATURES']
-    shuffle(features)
-    shuffle(promising)
-    features[0] = promising[0]
-    with open('{0}_swap_{1}.json'.format(features[0],promising[0]),'w') as f:
-        newjson['FEATURES']=features
-        json.dump(newjson,f)
+    # define sampled json
+    prevsample = copy.deepcopy(start)
 
+    # initialise auc
+    prevauc = startauc
+
+    converged = False
+    # will decide what constitutes converged later
+    while not converged:
+
+        sample = copy.deepcopy(prevsample)
+        # Sample a new hdf5 and replace existing at random
+        #   Or, just push it in, or just drop a hdf5 at random
+        u = np.random.rand()
+        if u < 0.25:
+            # drop an element at random
+            features = sample['features'][:]
+            random.shuffle(features)
+            dropped = features.pop()
+            sample['FEATURES'] = features
+        elif u > 0.25 and u < 0.5:
+            # push a new feature, but don't remove an old one
+            newfeature = random.sample(featlist,1)[0]
+            newmod = random.sample(modlist,1)[0]
+            sample['FEATURES'].append('{0}_{1}_'.format(modtyp, feature))           
+        elif u > 0.5:
+            # push a new feature and remove an old one
+            features = sample['features'][:]
+            random.shuffle(features)
+            dropped = features.pop()
+            newfeature = random.sample(featlist,1)[0]
+            newmod = random.sample(modlist,1)[0]
+            features.append('{0}_{1}_'.format(modtyp, feature))
+            sample['FEATURES'] = features
+
+        # ensure that ordering of the features is the same between jsons
+        sample['FEATURES'].sort()
+
+        # Then save this new json with a descriptive name
+        # unless it's already been generated
+        md5name = hashlib.md5("".join(sample['FEATURES']).encode('UTF-8')).hexdigest()
+        # get a list of the files in the mcmcdir
+        existingjsons = glob.glob(mcmcdir+"/*.json")
+        # check if the md5 exists
+        if md5name + ".json" in existingjsons:
+            # then load the results of that run
+            with open(os.path.join(mcmcdir,"AUC_scores.csv"),"r") as fh:
+                c = csv.reader(fh, delimiter="\t")
+                for line in c:
+                    # look for that md5sum
+                    if md5name in line[0]:
+                        auc_score = line[-1]
+        else:
+            # save a json with this name and run train.py on it
+            with open(os.path.join(mcmcdir,md5name+".json"), "w") as fh:
+                json.dump(sample, fh)
+            # call train.py
+            auc_score_dict = train(sample,verbose=verbose)
+            auc_score = auc_score_dict['all']
+
+        # compute acceptance probability from AUC:
+        #     r = min(1,AUC/(previous AUC))
+        acceptance = np.max([np.min([1,(auc_score-0.5)/(prevauc-0.5)]), 0]
+        # save current auc
+        prevauc = auc_score
+
+        u = np.random.rand()
+        # accept new point with probability r
+        if u < acceptance:
+            prevsample = sample
+        
+        #otherwise it will not overwrite prevsample, so continue from where it was
 
 if __name__ == "__main__":
     main()
