24a1339b8bb11c68acbcfefd6ad013a3d407d8d0
Gavin <gavingray1729@gmail.com>
2014-11-13 19:35:42 +0000

    fixed some comments

--- predict.py
+++ predict.py
@@ -8,34 +8,34 @@ import csv
 
 def main(settings_file='SETTINGS.json'):
 
-    #load the settings
+    # load the settings
     settings = utils.get_settings(settings_file)
 
     subjects = settings['SUBJECTS']
     features = settings['FEATURES']
 
-    #load the data
+    # load the data
     data = utils.get_data(settings)
 
-    #load the metadata
+    # load the metadata
     metadata = utils.get_metadata()
 
     features_that_parsed = list(data.keys())
     settings['FEATURES'] = [feature for feature in settings['FEATURES'] \
             if feature in features_that_parsed]
-    #iterate over subjects
+    # iterate over subjects
     prediction_dict = {}
 
     for subject in subjects:
-        #load the trained model:
+        # load the trained model:
         model = utils.read_trained_model(subject, settings, verbose=opts.verbose)
 
-        #initialise the data assembler
+        # initialise the data assembler
         assembler = utils.DataAssembler(settings, data, metadata)
-        #build test set
+        # build test set
         X = assembler.build_test(subject)
 
-        #make predictions
+        # make predictions
         predictions = model.predict_proba(X)
         for segment, prediction in zip(assembler.test_segments, predictions):
             prediction_dict[segment] = prediction
