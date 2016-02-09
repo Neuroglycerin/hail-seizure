6d2f8cd4e691a365b04c14282ad4eb8a28f0afc3
Scott Lowe <scott.code.lowe@gmail.com>
2016-02-09 15:01:08 +0000

    Change preprocess_feat to generate all necessary features

--- preprocess_feat.m
+++ preprocess_feat.m
@@ -3,23 +3,28 @@
 % Recursively add all of matlab folder to matlab path
 addpath(genpath('matlab'));
 
-logfname = fullfile('train',sprintf('featureprocessinglog-%s.txt',datestr(now,30)));
+logfname = fullfile('train', ...
+    sprintf('featureprocessinglog-%s.txt',datestr(now,30)));
 
-subj = subjnames();
-subj = {'Patient_2'};
+subjname_list = subjnames();
 
 % modtyps = {'raw','ica'};
 % modtyps = {'ica'};
 % modtyps = {'csp'};
 % modtyps = {'cln,raw,dwn','ica,raw,dwn','cln,csp,dwn'};
 % modtyps = {'cln,raw,dwn'};
-modtyps = {'cln,csp,dwn'};
+modtyps = {
+    'cln,raw,dwn';
+    'cln,ica,dwn';
+    'cln,csp,dwn';
+    'cln,cspdr,ica,dwn';
+};
 
 % ictypes = {'preictal'; 'interictal'; 'test'};
 % ictypes = {'pseudopreictal'; 'pseudointerictal';};
 ictypes = {'preictal'; 'interictal'; 'test'; 'pseudopreictal'; 'pseudointerictal';};
 
-nSplits = 10;
+nSplits_list = [1 10];
 
 feature_funcs = {
     @feat_var;
@@ -53,22 +58,25 @@ feature_funcs = {
     @feat_FFT;
     @feat_psd;
     @feat_coher;
-    };
+};
 
 matlabpool('local',12);
 
+for nSplits = nSplits_list(:)'
 for iMod = 1:numel(modtyps)
 modtyp = modtyps{iMod};
 for iFun = 1:numel(feature_funcs)
     tic2 = tic;
     fid = fopen(logfname,'a');
-    fprintf(fid,'%s %s: starting to compute %d %s on %s\n',datestr(now,30),getComputerName(),nSplits,func2str(feature_funcs{iFun}), modtyp);
+    fprintf(fid,'%s %s: starting to compute %d %s on %s\n', ...
+        datestr(now,30), getComputerName(), nSplits, func2str(feature_funcs{iFun}), modtyp);
     fclose(fid);
-    for iSub=1:numel(subj)
+    for iSub=1:numel(subjname_list)
         for iIct = 1:numel(ictypes)
-            fprintf('%s %s: Running feature %d %s on %s %s %s\n', getComputerName(), datestr(now,30), nSplits, func2str(feature_funcs{iFun}), modtyp, subj{iSub}, ictypes{iIct});
+            fprintf('%s %s: Running feature %d %s on %s %s %s\n', ...
+                getComputerName(), datestr(now,30), nSplits, func2str(feature_funcs{iFun}), modtyp, subjname_list{iSub}, ictypes{iIct});
             tic1 = tic;
-            getFeatAddToHDF5(feature_funcs{iFun}, subj{iSub}, ictypes{iIct}, modtyp, nSplits)
+            getFeatAddToHDF5(feature_funcs{iFun}, subjname_list{iSub}, ictypes{iIct}, modtyp, nSplits)
             tme = toc(tic1);
             tme = tme/60/60;
             hrs = floor(tme);
@@ -77,12 +85,12 @@ for iFun = 1:numel(feature_funcs)
             mins = floor(mins);
             fprintf('took %d h %d m %d s \n',hrs,mins,secs);
         end
-	% Add mutual information to HDF5 as well
+	    % Add mutual information to HDF5 as well
         myfeat = func2str(feature_funcs{iFun});
         if nSplits~=1
             myfeat = [num2str(nSplits) myfeat];
         end
-	% computeInfoAddToHDF5(myfeat, subj{iSub}, modtyp);
+	% computeInfoAddToHDF5(myfeat, subjname_list{iSub}, modtyp);
     end
     tme = toc(tic2);
     tme = tme/60/60;
@@ -90,10 +98,13 @@ for iFun = 1:numel(feature_funcs)
     mins = (tme-hrs)*60;
     secs = round(mod(mins,1)*60);
     mins = floor(mins);
-    fprintf('Feature %d %s took %d h %d m %d s \n',nSplits,func2str(feature_funcs{iFun}),hrs,mins,secs);
+    fprintf('Feature %d %s took %d h %d m %d s \n', ...
+        nSplits, func2str(feature_funcs{iFun}), hrs, mins, secs);
     fid = fopen(logfname,'a');
-    fprintf(fid,'%s %s: %d %s %s took %.1f seconds\n',datestr(now,30),getComputerName(),nSplits,func2str(feature_funcs{iFun}), modtyp, toc(tic2));
+    fprintf(fid,'%s %s: %d %s %s took %.1f seconds\n', ...
+        datestr(now,30), getComputerName(), nSplits, func2str(feature_funcs{iFun}), modtyp, toc(tic2));
     fclose(fid);
+end
 end
 end
 
