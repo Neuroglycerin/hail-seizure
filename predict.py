e7c6323369b8d2781a7f6b7cad5200012c7c9b4a
Scott Lowe <scott.code.lowe@gmail.com>
2016-02-15 17:27:37 +0000

    BUG: Also swap predict.py to use argparse

--- predict.py
+++ predict.py
@@ -76,6 +76,6 @@ def main(settings_file='SETTINGS.json'):
 if __name__ == '__main__':
 
     parser = utils.get_parser()
-    (opts, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    main(settings_file=opts.settings)
+    main(settings_file=args.settings)
